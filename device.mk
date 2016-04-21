7686ee2dd2fc2f791dfd51bdc7c27a8a3b0f3516
Aaron Kling <webgeek1234@gmail.com>
2016-04-20 21:39:22 -0500

    Vendor location of a touch file changed.

--- device.mk
+++ device.mk
@@ -103,7 +103,7 @@ PRODUCT_COPY_FILES += \
     $(OUT)/system/lib/libm.so:recovery/root/system/lib/libm.so \
     $(OUT)/system/lib/libstdc++.so:recovery/root/system/lib/libstdc++.so \
     vendor/nvidia/roth/proprietary/lib/librm31080.so:recovery/root/system/lib/librm31080.so \
-    vendor/nvidia/roth/proprietary/lib/hw/ts.default.so:recovery/root/system/lib/hw/ts.default.so
+    vendor/nvidia/roth/proprietary/vendor/lib/hw/ts.default.so:recovery/root/system/lib/hw/ts.default.so
 
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
 PRODUCT_COPY_FILES += \
