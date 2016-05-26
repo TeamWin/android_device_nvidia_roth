5c4aeca71c9ffe842f30b56939f830a05cb449b2
Aaron Kling <webgeek1234@gmail.com>
2016-05-26 13:16:15 -0500

    Copy touch files into the device tree

--- device.mk
+++ device.mk
@@ -123,8 +123,8 @@ PRODUCT_COPY_FILES += \
     $(OUT)/system/lib/liblog.so:recovery/root/system/lib/liblog.so \
     $(OUT)/system/lib/libm.so:recovery/root/system/lib/libm.so \
     $(OUT)/system/lib/libstdc++.so:recovery/root/system/lib/libstdc++.so \
-    vendor/nvidia/roth/proprietary/lib/librm31080.so:recovery/root/system/lib/librm31080.so \
-    vendor/nvidia/roth/proprietary/vendor/lib/hw/ts.default.so:recovery/root/system/lib/hw/ts.default.so
+    $(LOCAL_PATH)/twrp/librm31080.so:recovery/root/system/lib/librm31080.so \
+    $(LOCAL_PATH)/twrp/ts.default.so:recovery/root/system/lib/hw/ts.default.so
 
 # Wifi
 PRODUCT_PACKAGES += \
